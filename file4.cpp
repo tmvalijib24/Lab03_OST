e20b90889b2d676c7f25b9c333a72b7b151e6ebb
tmvalijib24 <tmvaliji_b24@it.vjti.ac.in>
2025-08-14 10:41:47 +0530

    adding my next file.

---
+++ file4.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int regId = 241080071;
+  cout << "Hello World" << endl;
+  cout << "My registration number is: " << regId << endl;
+  return 0;
+}
